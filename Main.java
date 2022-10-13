0c1037934d0282fb11e2e8fd2532dc2d190ebdfb
GREED <96002863+Sin0fGREED@users.noreply.github.com>
2022-10-13 16:37:27 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Main {
+public class hj {
 
     public static void formality() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello human!");
+        System.out.println("Hi!");
         System.out.println("what is your name?");
         String name = scanner.nextLine();
         System.out.println("Hello " + name + "!");
@@ -12,29 +12,31 @@ public class Main {
         System.out.println("How old are you?");
         int age = scanner.nextInt();
         if  (age >= 90) {
-            System.out.println("ON GOD, STOP THE CAP!");
+            System.out.println("STOP THE CAP!");
             System.out.println("How old are you!");
-        }else if (age <= 10){
-                System.out.println("GROW UP!");
+        }
+        else if (age <= 10){
+            System.out.println("GROW UP!");
             System.out.println("How old are you!");
         }
         System.out.println("Sorry, How old are you?");
-            int age3 = scanner.nextInt();
-            System.out.println("Oh, so you're " + age3 + " years old.");
-            System.out.println("Good to know.");
-            scanner.nextLine();
-            System.out.println("What are your hobby's?");
-            String hobby = scanner.nextLine();
-            System.out.println("oh so you like " + hobby + " that's interesting.");
-
-    }
+        int age3 = scanner.nextInt();
+        System.out.println("Oh, so you're " + age3 + " years old.");
+        System.out.println("Good to know.");
+        scanner.nextLine();
+        System.out.println("What are your hobby's?");
+        String hobby = scanner.nextLine();
+        System.out.println("oh so you like " + hobby + " that's interesting.");
+        System.out.println("Do you wanna play a game?");
+        System.out.println("It's really simple.");
+        }
 
     public static void game() {
         Scanner player = new Scanner(System.in);
 
         // prompt player for guess
-        System.out.println("Guess a number between 1 and 100");
-        guess = player.nextInt();
+        System.out.println("Just guess a number between 1 and 100");
+        int guess = player.nextInt();
         while (guess != NUMBER) {
             System.out.println("Guess again");
             guess = player.nextInt();
@@ -46,14 +48,25 @@ public class Main {
             } else if (guess < NUMBER) {
                 System.out.println("Too low");
             } else {
-                System.out.println("number is " + String.valueOf(NUMBER)); //DEBUG
+                System.out.println("number is " + NUMBER); //DEBUG
                 System.out.println("That's right!");
-                System.out.println("Congratulations you won absolutely nothing and wasted your time.");
+                System.out.println("Congratulations, You won absolutely nothing and wasted your time.");
                 System.exit(0);
             }
         }
     }
 
+
+
+    private static final Random Random = new Random();
+    private static final int NUMBER = Random.nextInt(100) + 1;
+
+    public static void main(String[] args) {
+        formality();
+        game();
+
+    }
+}
 
     private static final Random Random = new Random();
     private static final int NUMBER = Random.nextInt(100) + 1;
